1ef4ff00c60dfd72bd7bd56aee6a33d6707c3660
Dmitry Sagaidak <dm.sagaidak@gmail.com>
2023-05-09 10:46:17 +0600

    Added basic styles for user profile page

--- frontend/src/features/users/Profile.tsx
+++ frontend/src/features/users/Profile.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
 import { useAppSelector } from '../../app/hooks';
 import { selectUser } from './usersSlice';
+import { Container, Divider, Grid, Typography } from '@mui/material';
+import { apiUrl } from '../../constants';
+import { Navigate } from 'react-router-dom';
+import noAvatar from '../../../src/assets/images/no-avatar.png'
 
 const Profile = () => {
   const user = useAppSelector(selectUser);
-  console.log(user)
+
+  if(!user) {
+    return <Navigate to={'/login'}/>
+  }
+
   return (
-    <div>
-      {user?.displayName}
-    </div>
+    <Container>
+      <Grid container xs direction='column'>
+        <Typography variant="h4">{user?.displayName}</Typography>
+        <Divider/>
+        <Grid item container xs sx={{pt: 3}}>
+          {user.avatar ?
+            (<Typography
+            component="img"
+            src={apiUrl + '/' + user?.avatar}
+            alt={user?.displayName}
+            style={{width: '200px', height: '200px', borderRadius: '3px'}}
+          />) :
+            (<Typography
+              component="img"
+              src={noAvatar}
+              alt={user.displayName}
+              style={{width: '200px', height: '200px', borderRadius: '3px'}}
+            />)}
+
+          <Grid item sx={{ml: 2}}>
+            <Typography component="p">{user?.displayName}</Typography>
+            <Typography component="p">{user?.email}</Typography>
+            <Typography component="p">{user?.organization}</Typography>
+          </Grid>
+
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
