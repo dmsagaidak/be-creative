836841e7845d176c1f5666cbafdd552cdec69ea3
Dmitry Sagaidak <dm.sagaidak@gmail.com>
2023-05-30 11:37:24 +0600

    Updated adaptivity of Profile.tsx component

--- frontend/src/features/users/Profile.tsx
+++ frontend/src/features/users/Profile.tsx
@@ -31,11 +31,25 @@ const Profile = () => {
 
   console.log(tasks)
 
+  let userImg;
+
+  if (user?.avatar && user?.googleId){
+    const avatarUrl = user.avatar.slice(8);
+    userImg = apiUrl + '/' + avatarUrl;
+  } else {
+    userImg = apiUrl + '/' + user?.avatar;
+  }
+
   return (
     <Container>
       <Grid container direction='column'>
         <Grid item container>
-          <Typography variant="h4">{profileUser?.displayName}</Typography>
+          <Typography
+            variant="h4"
+            fontSize={{xs: '1.8rem', sm: '2rem'}}
+          >
+            {profileUser?.displayName}
+          </Typography>
           {user?._id === profileUser?._id &&
             (<>
                 <IconButton onClick={() => navigate(`/edit-user/${profileUser?._id}`)}>
@@ -47,24 +61,51 @@ const Profile = () => {
             </>)}
         </Grid>
         <Divider/>
-        <Grid item container sx={{pt: 3, pb: 3}}>
-          {profileUser?.avatar ?
-            (<Typography
-            component="img"
-            src={apiUrl + '/' + profileUser?.avatar}
-            alt={profileUser?.displayName}
-            style={{width: '200px', height: '200px', borderRadius: '3px'}}
-          />) :
-            (<Typography
-              component="img"
-              src={noAvatar}
-              alt={profileUser?.displayName}
-              style={{width: '150px', height: '150px', borderRadius: '5px'}}
-            />)}
-
-          <Grid item sx={{ml: 2}}>
-            <Typography component="p" fontSize="25px">Email: {profileUser?.email}</Typography>
-            <Typography component="p" fontSize="25px">Works at {profileUser?.organization}</Typography>
+        <Grid
+          item
+          container
+          sx={{pt: 3, pb: 3}}
+        >
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            md={3}
+            textAlign="center">
+            {profileUser?.avatar ?
+              (<Typography
+                component="img"
+                src={userImg}
+                alt={profileUser?.displayName}
+                style={{width: '200px', height: '200px', borderRadius: '3px'}}
+              />) :
+              (<Typography
+                component="img"
+                src={noAvatar}
+                alt={profileUser?.displayName}
+                style={{width: '150px', height: '150px', borderRadius: '5px'}}
+              />)}
+          </Grid>
+          <Grid
+            item
+            sx={{ml: 2}}
+            xs={12}
+            sm={6}
+            md={8}
+            textAlign={{xs: 'center', sm: 'left'}}
+          >
+            <Typography
+              component="p"
+              sx={{ml: 2}}
+              fontSize={{xs: '18px', sm: '20px', md: '25px'}}>
+              Email: {profileUser?.email}
+            </Typography>
+            <Typography
+              component="p"
+              sx={{ml: 2}}
+              fontSize={{xs: '18px', sm: '20px', md: '25px'}}>
+              Works at {profileUser?.organization}
+            </Typography>
           </Grid>
         </Grid>
         <Grid item container direction="column">
